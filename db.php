1a9ebd18f52dad00fe4d193f9972c38592ff6b0e
AndreaC BC <bc.andreac@gmail.com>
2024-01-16 13:07:52 -0500

    Set up DB

--- db.php
+++ db.php
@@ -1,13 +1,8 @@
 <?php
 $conn = mysqli_connect(
-    'root@127.0.0.1:3308',
+    'localhost:3308',
     'root',
-    'your_password',
+    '',
     'php_mysql_crud'
 );
-
-if (isset($conn)) {
-    echo 'DB is connected';
-}
-
 ?>
